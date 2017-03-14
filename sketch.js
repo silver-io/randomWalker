431fc285ed9fc5fea564db1d22c2b59bfe634cdc
silver-io <javier@silver.io>
2017-03-14 00:19:58 +0000

    refactors and code, we can now rotate the vector

--- sketch.js
+++ sketch.js
@@ -14,14 +14,13 @@ function draw() {
 function Walker() {
 	this.pos = createVector(width/2, height/2); // the ellipse starts from the centre of the screen
 	this.vel = createVector(0, 0);
+	this.acc = p5.Vector.fromAngle(random(TWO_PI));
+	this.acc.setMag(0.2);
+
 
 
 	this.update = function() {
-	  var mouse = createVector(mouseX, mouseY);
-	  this.acc = p5.Vector.sub(mouse, this.pos) //the mouse position is stored in acc
-	  this.acc.mult(0.01);
-
-
+	  this.acc.rotate(0.1);
 	  this.vel.add(this.acc); //acceleration changes velocity
 	  this.pos.add(this.vel); //velocity changes position
 
